56fcc72a6e219b4f820cf617ca184f18cfab5fd4
Sulav Thapa <54358760+sulavthapa19@users.noreply.github.com>
2025-12-29 16:40:50 +0200

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -33,17 +33,17 @@ CREATE VIEW gold.dim_customers
 AS
 SELECT
     ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
-    ci.cst_id                           AS customer_id,
-    ci.cst_key                          AS customer_number,
-    ci.cst_firstname                    AS first_name,
-    ci.cst_lastname                     AS last_name,
-    ci.cst_marital_status               AS marital_status,
-    la.CNTRY                            AS country,
+    ci.cst_id AS customer_id,
+    ci.cst_key AS customer_number,
+    ci.cst_firstname AS first_name,
+    ci.cst_lastname AS last_name,
+    ci.cst_marital_status AS marital_status,
+    la.CNTRY AS country,
     CASE
-        WHEN ci.cst_gndr != 'Unknown' THEN ci.cst_gndr
+        WHEN ci.cst_gndr != 'Unknown' THEN ci.cst_gndr --CRM is the primary source
         ELSE COALESCE(ca.gen, 'Unknown')
-    END                                 AS gender,
-    ca.bdate                            AS birth_date,
+    END AS gender,
+    ca.bdate AS birth_date,
     ci.cst_create_date
 FROM silver.crm_cust_info AS ci
 LEFT JOIN silver.erp_cust_az12 AS ca
@@ -62,16 +62,16 @@ GO
 CREATE VIEW gold.dim_products AS
 SELECT
     ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
-    pn.prd_id                                                AS product_id,
-    pn.prd_key                                               AS product_number,
-    pn.prd_nm                                                AS product_name,
-    pn.cat_id                                                AS category_id,
-    pc.cat                                                   AS category,
-    pc.subcat                                                AS subcategory,
+    pn.prd_id  AS product_id,
+    pn.prd_key AS product_number,
+    pn.prd_nm  AS product_name,
+    pn.cat_id AS category_id,
+    pc.cat AS category,
+    pc.subcat AS subcategory,
     pc.maintenance,
-    pn.prd_cost                                              AS product_cost,
-    pn.prd_line                                              AS product_line,
-    pn.prd_start_dt                                          AS product_start_date
+    pn.prd_cost AS product_cost,
+    pn.prd_line AS product_line,
+    pn.prd_start_dt AS product_start_date
 FROM silver.crm_prd_info AS pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
     ON pn.cat_id = pc.id
